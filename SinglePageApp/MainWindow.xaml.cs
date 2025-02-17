0fdbd69f177e9d711c290f2f3ddc7697d31b3900
Szegedi Barnabás <szeveba@gmail.com>
2025-02-17 11:04:58 +0100

    SPA-MPA sample added

---
+++ SinglePageApp/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using SinglePageApp.View;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SinglePageApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            MainFrame_Click(sender, e);
+        }
+
+        private void SecondFrame_Click(object sender, RoutedEventArgs e)
+        {
+            var secondPage = new SecondaryPage();
+            MainFrame.Content = secondPage;
+        }
+
+        private void MainFrame_Click(object sender, RoutedEventArgs e)
+        {
+            var mainPage = new MainPage();
+            MainFrame.Content = mainPage;
+        }
+
+        private void ShowWindow_Click(object sender, RoutedEventArgs e)
+        {
+            var w = new SampleWindow();
+            w.Show();
+        }
+
+        private void ShowDialog_Click(object sender, RoutedEventArgs e)
+        {
+            var w = new SampleWindow();
+            w.ShowDialog();
+        }
+    }
+}
